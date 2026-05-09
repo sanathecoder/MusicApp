b6e88c21caea6201bee0c1315ca831dcbc58171e
sanathecoder <sanasana33377737@gmail.com>
2026-05-09 15:01:56 +0500

    error resolve

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' // Ye v4 ke liye hai
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(), // Ye plugin yahan hona lazmi hai
+  ],
   build: {
     outDir: 'dist', 
   }
